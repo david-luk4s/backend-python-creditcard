8f201c7c1fe470040403f22d2911646f24ecb90a
David Lucas <davidlucaslks@gmail.com>
2023-04-10 12:16:29 -0300

    feat: created app serializer for user

---
+++ application/user.py
@@ -0,0 +1,35 @@
+from jsonpickle import decode
+
+from domain.entities.user import User
+from domain.repos.user import RepositoryUser
+
+from adapters.infrastructure.postgresql.user import UserImpl
+
+class AuthSerializer:
+    """Auth serializer for user."""
+
+    username: str
+    password: str
+
+    def __init__(self, data: any) -> None:
+        if data is not None:
+            data = decode(data.decode())
+
+            self.username = data.get("username")
+            self.password = data.get("password")
+
+
+class UserSerializer:
+    username: str
+    password: str
+
+    def __init__(self, username: str, password: str) -> None:
+        self.username = username
+        self.password = password
+
+    def save(self) -> None:
+        from main import DB as POSTGRES_DB
+
+        user = User(self.username, self.password)
+        repo = RepositoryUser(UserImpl(POSTGRES_DB))
+        repo.service_save(user)
